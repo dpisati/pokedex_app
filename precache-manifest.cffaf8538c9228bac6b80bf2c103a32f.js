c065b1ecbfd9e827055120e9c2ecb9adb49609fa
dpisati <43629737+dpisati@users.noreply.github.com>
2020-01-10 11:30:41 +1300

    Updates

---
+++ precache-manifest.cffaf8538c9228bac6b80bf2c103a32f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84aac9eb89519d7953110b2b01537cd1",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "768d100f196a63e26e5e",
+    "url": "/pokedex/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "3b7b2af5d06237771dd2",
+    "url": "/pokedex/static/css/main.a52c3542.chunk.css"
+  },
+  {
+    "revision": "768d100f196a63e26e5e",
+    "url": "/pokedex/static/js/2.e7578842.chunk.js"
+  },
+  {
+    "revision": "7eb906b2d8ef627da08d7f4a543e6732",
+    "url": "/pokedex/static/js/2.e7578842.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3b7b2af5d06237771dd2",
+    "url": "/pokedex/static/js/main.f824dec3.chunk.js"
+  },
+  {
+    "revision": "e600d5e7cf25022d5d6a",
+    "url": "/pokedex/static/js/runtime-main.28ab4164.js"
+  },
+  {
+    "revision": "b2dde4eb8f10fe8d8c0ccc7dff652a15",
+    "url": "/pokedex/static/media/background_pikachu.b2dde4eb.jpg"
+  },
+  {
+    "revision": "ae5a1cb9cd87bda27d42d52241bcd35c",
+    "url": "/pokedex/static/media/pokedex_backgroup.ae5a1cb9.png"
+  },
+  {
+    "revision": "65f455a87a700165a828fd6bc02dff7f",
+    "url": "/pokedex/static/media/pokedex_backgroup_mobile.65f455a8.png"
+  },
+  {
+    "revision": "6f1fd44625c52ccd693ca4e39b23cc9f",
+    "url": "/pokedex/static/media/pokedex_logo.6f1fd446.png"
+  },
+  {
+    "revision": "09ced8ac37d2421a505b5cbb48d1eddb",
+    "url": "/pokedex/static/media/spinner.09ced8ac.gif"
+  }
+]);
